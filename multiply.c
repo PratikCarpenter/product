87ad6fea8a23a6b28346bab0f8a3e59efcd881d5
PratikCarpenter <pratikcarpenter7869@gmail.com>
2023-05-11 23:39:47 +0530

    added multiply

---
+++ multiply.c
@@ -0,0 +1,10 @@
+
+
+#include<stdio.h>
+void main()
+{   int a,b,multi=0;
+    printf("enter the value of a and b");
+   scanf("%d%d",&a,&b);
+   multi=a*b;
+   printf(" MULTI of A and B ..%d ", multi);
+}
